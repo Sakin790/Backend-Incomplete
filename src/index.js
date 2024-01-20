de4d05efe691293e429aa6714bdc9049af435290
Sakin790 <sakinislam79@gmail.com>
2024-01-20 15:17:40 +0600

    server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 9090, () => {
+      console.log(`Server is Running at Port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoBD Connection failed", error);
+  });
