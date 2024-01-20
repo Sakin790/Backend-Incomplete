9124e9aea1f2108e252de2423e46c7b8fecb0213
Sakin790 <sakinislam79@gmail.com>
2024-01-20 18:17:08 +0600

    How to recived data frm user

--- src/app.js
+++ src/app.js
@@ -3,11 +3,22 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 
-
+//This is corse origin resoure sharing configuration
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
+
+//Max 16kb json data recive korbo
+app.use(express.json({ limit: "16kb" }));
+//URL theke data neyar jonno
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+app.use(cookieParser());
 export { app };
