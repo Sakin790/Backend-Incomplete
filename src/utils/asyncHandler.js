f541d8ebfdaffa530b88c29f8882300f43174efe
Sakin790 <sakinislam79@gmail.com>
2024-01-21 12:41:06 +0600

    multer

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 const asyncHandler = (requestHandle) => {
-    return (req, res, next) => {
-       Promise.resolve(requestHandle(req, res, next)).catch((err) => next(err));
-     };
-   };
-   
-   export { asyncHandler };
+  return (req, res, next) => {
+    Promise.resolve(requestHandle(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
